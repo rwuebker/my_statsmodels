f51edfaa3db9e1042bdc1e1fabde57bd5b9baf99
Richard Wuebker <richardwuebker@Richards-MacBook-Air.local>
2021-04-10 00:18:06 -0400

    imports OLS

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,2 +1,15 @@
+import os
+import sys
+import numpy as np
+
+p = os.getcwd()
+if p not in sys.path:
+    sys.path.insert(0, p)
+
+from models import OLS
+
+
 def test_ols():
+
+    rng = np.random.default_rng(12345)
     assert True
